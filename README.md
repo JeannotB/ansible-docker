44c192da7b754c6eb065b4fa6f07f8c2d8621a4f
Baptiste Chevallier <baptiste.chevallier@mythalesgroup.io>
2020-01-30 08:51:49 +0100

    Add docker deployment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ansible-docker
+
+Source : 
+ - https://blog.dev-web.io/2019/02/10/utiliser-symfony-dans-docker/
+ - https://github.com/geerlingguy
--- deployment.sh
+++ deployment.sh
@@ -1,2 +1,2 @@
 #Install docker
-ansible-playbook remote/playbook.yml -i deployment/hosts -u "webserver" --ask-pass --ask-sudo-pass
+ansible-playbook remote/playbook.yml -i deployment/hosts -u "webserver" --ask-pass --ask-become-pass -e "ansible_python_interpreter=/usr/bin/python3"
